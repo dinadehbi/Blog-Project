b2f283e682adec0a8416b3a5361e029756202e0c
Dina Dehbi <dinadh012@gmail.com>
2025-03-11 11:21:00 +0000

    Multi langue Blog

--- Projet-Blog-VueJs/resources/js/app.js
+++ Projet-Blog-VueJs/resources/js/app.js
@@ -1,11 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
 import { createApp } from 'vue';
-
 import App from './components/App.vue';
-
 import router from './router/router.js';
-  
-createApp(App).use(router).mount('#app');
+import { createI18n } from 'vue-i18n';
+import en from './locales/en.js';
+import ar from './locales/ar.js';
+
+// Create an instance of Vue I18n
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',  // Default language
+  messages: {
+    en,
+    ar,
+  },
+});
+
+createApp(App)
+  .use(router)
+  .use(i18n)  // Add Vue I18n to the app
+  .mount('#app');
---
+++ Projet-Blog-VueJs/resources/js/locales/en.js
@@ -0,0 +1,14 @@
+// resources/js/locales/en.js
+export default {
+    welcome: 'Welcome to Our Blog',
+    home: 'Home',
+    articles: 'Articles',
+    categories: 'Categories',
+    about: 'About',
+    contact: 'Contact',
+    authOptions: 'Auth Options',
+    login: 'Login',
+    register: 'Register',
+    logout: 'Logout',
+  };
+  
